7ab0c498958392a351ad60220f56fc8420d4178d
Juan Luna Ramirez <fclunar@gmail.com>
2020-02-03 19:07:25 -0800

    refactor: switch Button to styled components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,22 +1,15 @@
-import React from 'react';
+import styled from 'styled-components';
 
-const styles = {
-  primary: ' bg-white border border-blue-600 text-blue-700 hover:bg-blue-600 hover:text-white',
-  secondary: 'bg-white border border-gray-100 text-gray-600',
-};
-
-const Button = props => {
-  const { children, className, buttonStyle, type } = props;
-
-  return (
-    // eslint-disable-next-line react/button-has-type
-    <button
-      type={type || 'button'}
-      className={`px-4 py-2 rounded-lg ${className} ${styles[buttonStyle] || styles.secondary}`}
-    >
-      {children}
-    </button>
-  );
-};
+const Button = styled.button`
+  appearance: none;
+  color: ${props => props.theme.colors.indigo_500};
+  border: 1px solid ${props => props.theme.colors.indigo_500};
+  padding: ${props => props.theme.spacing['1']} ${props => props.theme.spacing['2']};
+  font-size: ${props => props.theme.fontSize.lg};
+  border-radius: 5px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 export default Button;
