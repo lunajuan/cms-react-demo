80b35af9af3a46018c304ccee66a1bf64c5decf1
Juan Luna Ramirez <fclunar@gmail.com>
2020-02-05 07:04:15 -0800

    feature: add basic formik form
    Add basic formik form with simple input and textarea. Also some base
    styles.

---
+++ src/components/form.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Formik } from 'formik';
+import Button from './Button';
+
+const FormContainer = styled.form`
+  input,
+  textarea {
+    appearance: none;
+    border: 1px solid ${props => props.theme.colors.grey_100};
+    border-radius: 5px;
+    padding: 0;
+    line-height: inherit;
+    color: inherit;
+    display: block;
+    width: 100%;
+    padding: ${props => props.theme.spacing['2']} ${props => props.theme.spacing['3']};
+  }
+
+  textarea {
+    resize: none;
+    min-height: 200px;
+  }
+
+  .field-group {
+    display: block;
+    margin-bottom: ${props => props.theme.spacing['3']};
+  }
+
+  .field-label {
+    color: ${props => props.theme.colors.grey_600};
+  }
+`;
+
+// pass in id
+const form = () => {
+  return (
+    <Formik
+      initialValues={{ title: '', description: '' }}
+      onSubmit={values => {
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      {props => {
+        const {
+          values: { title, description },
+          errors,
+          handleSubmit,
+          handleChange,
+          handleBlur,
+        } = props;
+        return (
+          <FormContainer onSubmit={handleSubmit}>
+            <label htmlFor="title" className="field-group">
+              <span className="field-label">Title</span>
+              <input
+                type="text"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={title}
+                name="title"
+              />
+              {errors.title && <div id="feedback">{errors.title}</div>}
+            </label>
+
+            <label htmlFor="description" className="field-group">
+              <span className="field-label">Description</span>
+              <textarea
+                onChange={handleChange}
+                value={description}
+                name="description"
+                className="textarea"
+              />
+            </label>
+
+            <Button type="submit">Submit</Button>
+          </FormContainer>
+        );
+      }}
+    </Formik>
+  );
+};
+
+export default form;
