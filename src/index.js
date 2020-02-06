d7b4c221d635017ff90e7a100edff63992de2bec
Juan Luna Ramirez <fclunar@gmail.com>
2020-02-05 19:42:19 -0800

    refactor: route to all products and form
    Add router to view all products and form to add new products.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
 import theme from './styles/theme';
 import GlobalStyle from './styles/GlobalStyle';
 import Container from './components/Container';
+import Products from './components/Products';
+import Form from './components/form';
 
 const App = () => {
+  // set state to track all products
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Container>
-        <code>
-          <span role="img" aria-label="festive emoji">
-            🥳
-          </span>{' '}
-          Sample App Here!
-        </code>
+        <Router>
+          <Switch>
+            <Route path="/form">
+              <NavLink exact to="/">
+                All Products
+              </NavLink>
+              <Form />
+            </Route>
+            <Route>
+              <Products />
+            </Route>
+          </Switch>
+        </Router>
       </Container>
     </ThemeProvider>
   );
