86f3ad7b0e96cb3b52dce06a6a2d7cc301f86ce0
Juan Luna Ramirez <fclunar@gmail.com>
2020-03-29 13:31:05 -0700

    feature: use os prefered color scheme

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -20,7 +20,14 @@ const useDarkMode = () => {
 
   useEffect(() => {
     const localTheme = window.localStorage.getItem('theme');
-    if (localTheme) {
+
+    if (
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches &&
+      !localTheme
+    ) {
+      setMode('dark');
+    } else if (localTheme) {
       setTheme(localTheme);
     } else {
       setMode('light');
