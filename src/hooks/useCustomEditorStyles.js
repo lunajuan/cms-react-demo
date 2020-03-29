cc4ddbb55cf43aa8572d7bf4a1da69affffa6f1f
Juan Luna Ramirez <fclunar@gmail.com>
2020-03-29 13:18:44 -0700

    fix: remove editor color styles

--- src/hooks/useCustomEditorStyles.js
+++ src/hooks/useCustomEditorStyles.js
@@ -1,22 +1,7 @@
 import { useContext } from 'react';
 import { ThemeContext } from 'styled-components';
-import colors from '../styles/colors';
 // Color inline styles are mutually exclusive. Meaning they can't exist at the
 // same time so enabling one should disable all others.
-
-export const colorStyles = [
-  { label: 'Indigo 500', styleName: 'indigo_500', style: { color: colors.indigo_500 } },
-  { label: 'Red 500', styleName: 'red_500', style: { color: colors.red_500 } },
-  { label: 'Grey 900', styleName: 'grey_900', style: { color: colors.grey_900 } },
-  { label: 'Grey 800', styleName: 'grey_800', style: { color: colors.grey_800 } },
-  { label: 'Grey 700', styleName: 'grey_700', style: { color: colors.grey_700 } },
-  { label: 'Grey 600', styleName: 'grey_600', style: { color: colors.grey_600 } },
-  { label: 'Grey 500', styleName: 'grey_500', style: { color: colors.grey_500 } },
-  { label: 'Grey 400', styleName: 'grey_400', style: { color: colors.grey_400 } },
-  { label: 'Grey 300', styleName: 'grey_300', style: { color: colors.grey_300 } },
-  { label: 'Grey 200', styleName: 'grey_200', style: { color: colors.grey_200 } },
-  { label: 'Grey 100', styleName: 'grey_100', style: { color: colors.grey_100 } },
-];
 
 const getCustomSyleMapInstructions = getStyleValue => styles =>
   styles.reduce((customMap, style) => {
@@ -31,13 +16,17 @@ const useCustomEditorStyles = () => {
   const textColorStyles = Object.entries(textThemeProps).reduce(
     (styles, [styleName, colorValue]) => {
       const newStyles = styles;
+      // remove primary because it is the default text color. This will probably
+      // break easily so change to better solution
+      if (styleName === 'primary') return styles;
+
       newStyles.push({ label: `${styleName} text color`, styleName, style: { color: colorValue } });
       return newStyles;
     },
     []
   );
 
-  return { textColorStyles: [...textColorStyles, ...colorStyles], getCustomSyleMapInstructions };
+  return { textColorStyles, getCustomSyleMapInstructions };
 };
 
 export default useCustomEditorStyles;
